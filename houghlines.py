29545a6b95633fde774792cfea1697492091597b
Tony <tony@malzhacker.de>
2016-12-06 17:53:38 +0100

    vertical feature detection

--- houghlines.py
+++ houghlines.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 
+def plot(img):
+    plt.imshow(img, cmap='gray', interpolation='none')
+    plt.axis([0, img.shape[1], img.shape[0], 0])
+    plt.show()
 
 def find_horizontal_lines(image):
     lines = []
@@ -21,7 +25,7 @@ def find_horizontal_lines(image):
     # line rotation angle (theta):
     # 0 equals vertical lines
     # pi/2 (1.5708) equals horizontal lines
-    print('Found %s lines: ' % len(hlines))
+    print('Found %s horizontal lines: ' % len(hlines))
     for line in hlines:
         for rho, theta in line:
             # convert from polar to cartesian
@@ -32,56 +36,8 @@ def find_horizontal_lines(image):
     lines.sort()
     return lines
 
-def find_lines(image):
-    lines = []
-
-    pi = round(np.pi, 4)
-    acc_rho = 1  # pixel resolution
-    acc_theta = pi / 180  # angle resolution in radians
-    acc_threshold = 100  # min votes needed to get returned as a line (here min number of pixels)
-    # limit theta to an angle of max 90 degree
-    min_theta = 1.57
-    max_theta = 1.58
-    # print((max_theta - min_theta) / acc_theta)
-    hlines = cv2.HoughLines(image, acc_rho, acc_theta, acc_threshold, 0, 0, 0, min_theta, max_theta)
-
-    # line rotation angle (theta):
-    # 0 equals vertical lines
-    # pi/2 (1.5708) equals horizontal lines
-    print('Found %s lines: ' % len(hlines))
-    for line in hlines:
-        for rho, theta in line:
-            # convert from polar to cartesian
-            x = np.cos(theta)
-            y = np.sin(theta)
-            x0 = x * rho
-            y0 = y * rho
-            # get x, y coordinates of 2 separate points (distance 2000 units/pixels) of this line
-            # p1
-            x1 = int(x0 + 1000 * (-y))
-            y1 = int(y0 + 1000 * (x))
-            # p2
-            x2 = int(x0 - 1000 * (-y))
-            y2 = int(y0 - 1000 * (x))
-            lines.append([(x1,x2),(y1,y2),(x0,y0)])
-            print(rho, theta, x, y, x0, y0, x1, y1, x2, y2)
-    return lines
-
 img = cv2.imread('entchen.png', cv2.IMREAD_GRAYSCALE)
-# plt.imshow(img, cmap='gray')
-# plt.show()
 edges = cv2.Canny(img, 100, 200)
-# plt.imshow(edges[45:200, 70:290], cmap='gray')
-# plt.show()
-
-# staffLines = find_lines(edges)
-# plt.imshow(img, cmap='gray', interpolation='none')
-# plt.axis([0, 500, img.shape[0], 0])
-# if staffLines is not None:
-#     for coords in staffLines:
-#         print(coords)
-#         plt.plot(coords[0], coords[1], 'r-', linewidth=.5)
-# plt.show()
 
 def find_staff_lines(lines):
     assert (len(lines) % 5 == 0), "Did not detect all staff lines"
@@ -100,9 +56,7 @@ def find_staff_lines(lines):
     staffLines.pop()
     return staffLines
 
-# show all horizontal lines
 lines = find_horizontal_lines(edges)
-
 
 def plot_edges(lines):
     plt.imshow(img, cmap='gray', interpolation='none')
@@ -112,8 +66,6 @@ def plot_edges(lines):
             plt.plot((0, 1000), (yIntercept, yIntercept), 'r-', linewidth=.5)
     plt.show()
 
-
-# show all staff lines
 staffLines = find_staff_lines(lines)
 
 def plot_stafflines(staffLines):
@@ -124,7 +76,6 @@ def plot_stafflines(staffLines):
             yIntercept = line[2] # mean y value
             plt.plot((0, 1000), (yIntercept, yIntercept), 'g-', linewidth=1)
     plt.show()
-
 
 def get_avg_staff_height(stafflines):
     assert (len(lines) >= 5 ), "Did not detect all staff lines"
@@ -158,10 +109,6 @@ def get_row_coords(staffLines):
 rowCoords = get_row_coords(staffLines)
 print("Found %d rows" % len(rowCoords))
 
-
-
-
-
 def delete_staffline(img,row):
     _, imgCols = img.shape
     mostFreqValue = Counter(img[row].flat).most_common(1)[0][0]
@@ -183,14 +130,14 @@ for i in range(0, len(staffLines)):
     delete_staffline(img, int(staffMiddle + .5))
     delete_staffline(img, int(staffBottom + .5))
 
-    plt.plot((0, 1000), (staffTop, staffTop), 'r--', linewidth=1)
-    plt.plot((0, 1000), (staffMiddle, staffTop), 'g-', linewidth=1)
-    plt.plot((0, 1000), (staffBottom, staffBottom), 'r--', linewidth=1)
-
-
-plt.imshow(img, cmap='gray', interpolation='none')
-plt.axis([0, img.shape[1], img.shape[0], 0])
-plt.show()
+#     plt.plot((0, 1000), (staffTop, staffTop), 'r--', linewidth=1)
+#     plt.plot((0, 1000), (staffMiddle, staffTop), 'g-', linewidth=1)
+#     plt.plot((0, 1000), (staffBottom, staffBottom), 'r--', linewidth=1)
+#
+#
+# plt.imshow(img, cmap='gray', interpolation='none')
+# plt.axis([0, img.shape[1], img.shape[0], 0])
+# plt.show()
 
 def split_img_into_rows(img, rowCoords):
     rows = []
@@ -198,40 +145,80 @@ def split_img_into_rows(img, rowCoords):
         rows.append(img[int(rowY[0]):int(rowY[1])])
     return rows
 
-rows = split_img_into_rows(img, rowCoords)
-# for row in rows:
-#     plt.imshow(row, cmap='gray', interpolation='none')
-#     plt.show()
+
 
 bw = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, -2)
-plt.imshow(bw, cmap='gray', interpolation='none')
-plt.axis([0, bw.shape[1], bw.shape[0], 0])
-plt.show()
+
 
 blur = cv2.GaussianBlur(bw,  (3, 3), 0)
-plt.imshow(cv2.bitwise_not(blur), cmap='gray', interpolation='none')
-plt.axis([0, blur.shape[1], blur.shape[0], 0])
-plt.show()
+# plt.imshow(cv2.bitwise_not(blur), cmap='gray', interpolation='none')
+# plt.axis([0, blur.shape[1], blur.shape[0], 0])
+# plt.show()
 
 # opening = np.copy(img)
 # kernel = np.ones((5, 5), np.uint8)
 # opening = cv2.morphologyEx(opening, cv2.MORPH_GRADIENT, kernel)
-# plt.imshow(opening, cmap='gray', interpolation='none')
-# plt.axis([0, opening.shape[1], opening.shape[0], 0])
-# plt.show()
-#
+
 vertical = np.copy(blur)
 kernel = np.ones((3, 1), np.uint8)
 vertical = cv2.erode(vertical, kernel)
 vertical = cv2.dilate(vertical, kernel)
-plt.imshow(cv2.bitwise_not(vertical), cmap='gray', interpolation='none')
-plt.axis([0, vertical.shape[1], vertical.shape[0], 0])
-plt.show()
+
 
 _, bw = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-plt.imshow(cv2.bitwise_not(bw), cmap='gray', interpolation='none')
-plt.axis([0, bw.shape[1], bw.shape[0], 0])
-plt.show()
+
+def find_vertical_lines(image, staff_height):
+    lines = []
+
+    pi = round(np.pi, 4)
+    acc_rho = 1  # pixel resolution
+    acc_theta = pi / 180  # angle resolution in radians
+    acc_threshold = int(staff_height * 0.6)  # min votes needed to get returned as a line (here min number of pixels)
+    # limit theta to horizontal lines
+    min_theta = 0.
+    max_theta = 0.01
+    # print((max_theta - min_theta) / acc_theta)
+    hlines = cv2.HoughLines(image, acc_rho, acc_theta, acc_threshold, 0, 0, 0, min_theta, max_theta)
+
+
+    # line rotation angle (theta):
+    # 0 equals vertical lines
+    # pi/2 (1.5708) equals horizontal lines
+    print('Found %s vertical lines: ' % len(hlines))
+    for line in hlines:
+        for rho, theta in line:
+            # convert from polar to cartesian
+            x = np.cos(theta) * rho
+            # print(y, theta, rho)
+            lines.append(x)
+
+    lines.sort()
+    return lines
+
+print(get_avg_staff_height(staffLines))
+
+rows = split_img_into_rows(bw, rowCoords)
+for row in rows:
+    edges = cv2.Canny(cv2.bitwise_not(row), 100, 200)
+
+    vlines = find_vertical_lines(edges, get_avg_staff_height(staffLines))
+    for xVal in vlines:
+        xVal = int(xVal + 0.5)
+        plt.plot((xVal, xVal), (0, 500), 'r-')
+    plot(cv2.bitwise_not(row))
+
+# img = cv2.bitwise_not(img)
+# cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+# circles = cv2.HoughCircles(cv2.bitwise_not(blur), cv2.HOUGH_GRADIENT, 1, 10, param1=350, param2=10, minRadius=0, maxRadius=10)
+# for i in circles[0,:]:
+#     # draw the outer circle
+#     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+#     # draw the center of the circle
+#     cv2.circle(cimg,(i[0],i[1]),1,(0,0,255),3)
+#
+# cv2.imshow('detected circles', cimg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 '''   sadfa
